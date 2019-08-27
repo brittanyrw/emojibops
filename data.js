4284c9d7009395042751d395acc6da1697d1dbdc
Brittany Walker <breneewalker@gmail.com>
2019-08-27 15:04:15 -0400

    Update data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+const emojiItems = [
+    {
+        title: "I Wanna Dance with Somebody",
+        emojiImgs: "💃🏽🕺🏽🔥♥️",
+        artist: ["Whitney Houston"],
+        genres: ["pop", "rb"],
+        year: 1987
+    }
+]
