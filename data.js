a5d3921e1ef2663de249c281ce7fc39af8e86fde
Герасимов Алексей <gerasimov@tochkak.ru>
2019-09-27 13:17:01 +0300

    Added Stan by Eminem

--- data.js
+++ data.js
@@ -87,6 +87,14 @@ const emojiItems = [
         musicVideo: "https://www.youtube.com/watch?v=DtVBCG6ThDk",
         genres: ["soft rock"],
         year: 1972
+	},{
+        title: "Stan",
+        artist: ["Eminem"],
+        featuredArtist: ["Dido"],
+        emojiImgs: "✉️👱🏻🙍🏼👼🏻",
+        musicVideo: "https://www.youtube.com/watch?v=gOMhN-hfMtY",
+        genres: ["hip-hop", "horrorcore"],
+        year: 2000
     },{
         title: "Stone Cold",
         artist: ["Demi Lovato"],
