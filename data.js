2fb2d62ed409f899f62a6d78dbd2595811b9471a
SankalpSh <sankalpsh98@gmail.com>
2019-09-30 16:04:16 +0530

    Added Follow Your Arrow by Kacey Musgraves
    this PR closes #166

--- data.js
+++ data.js
@@ -48,6 +48,14 @@ const emojiItems = [
         genres: ["soundtrack", "pop"],
         year: 2003
     },{
+        title: "Follow Your Arror",
+        artist: ["Kacey Musgraves"],
+        featuredArtist: [],
+        emojiImgs: "➡️🏹🏜️👢👧",
+        musicVideo: "https://www.youtube.com/watch?v=kQ8xqyoZXCc",
+        genres: ["country"],
+        year: 2013
+    },{
         title: "I Wanna Dance with Somebody",
         artist: ["Whitney Houston"],
         featuredArtist: [],
