23802c249b85065e0def690d62c8755d34002d22
Imperial Owl <yogazacty@gmail.com>
2019-09-30 19:54:51 +0700

    Add I Want to Hold Your Hand by the Beatles

--- data.js
+++ data.js
@@ -118,8 +118,8 @@ const emojiItems = [
         emojiImgs: "🚚🎳🚓",
         musicVideo: "https://youtu.be/pafY6sZt0FE",
         genres: ["rock", "alternative"],
-  year: 2019
-},{
+        year: 2019
+    },{
         title: "Thriller",
         artist: ["Michael Jackson"],
         featuredArtist: [],
@@ -127,5 +127,13 @@ const emojiItems = [
         musicVideo: "https://www.youtube.com/watch?v=4V90AmXnguw",
         genres: ["pop"],
         year: 1984
+    },{
+        title: "I Want to Hold Your Hand",
+        artist: ["The Beatles"],
+        featuredArtist: [],
+        emojiImgs: "🙋‍♂️🤝🏾👩",
+        musicVideo: "https://www.youtube.com/watch?v=jenWdylTtzs",
+        genres: ["pop rock"],
+        year: 1963
     }
 ]
