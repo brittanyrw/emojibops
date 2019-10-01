2d55d4aa1c364d547772b1d66fc538e940dd8fd3
Sarawut Silprawat <sarawuts@tellscore.com>
2019-10-01 16:34:25 +0700

    Added new song emoji

--- data.js
+++ data.js
@@ -118,8 +118,8 @@ const emojiItems = [
         emojiImgs: "🚚🎳🚓",
         musicVideo: "https://youtu.be/pafY6sZt0FE",
         genres: ["rock", "alternative"],
-  year: 2019
-},{
+        year: 2019
+    },{
         title: "Thriller",
         artist: ["Michael Jackson"],
         featuredArtist: [],
@@ -127,5 +127,13 @@ const emojiItems = [
         musicVideo: "https://www.youtube.com/watch?v=4V90AmXnguw",
         genres: ["pop"],
         year: 1984
+    },{
+        title: "Smooth Criminal",
+        artist: ["Michael Jackson"],
+        featuredArtist: [],
+        emojiImgs: "🚶🏻‍♂️🌑",
+        musicVideo: "https://www.youtube.com/watch?v=h_D3VFfhvs4",
+        genres: ["pop"],
+        year: 1987
     }
 ]
