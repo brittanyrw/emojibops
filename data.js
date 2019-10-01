ae86688ea777900a42d6bf8fac1a6526ca3d4470
Brittany Walker <breneewalker@gmail.com>
2019-10-01 11:44:06 -0400

    update

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ const emojiItems = [
         musicVideo: "https://www.youtube.com/watch?v=fJ9rUzIMcZQ",
         genres: ["art-rock", "opera-rock", "classic-rock"],
         year: 1975
-    },{
+    }, {
         title: "Boy With Luv",
         artist: ["BTS"],
         featuredArtist: ["Halsey"],
@@ -15,7 +15,7 @@ const emojiItems = [
         musicVideo: "http://www.youtube.com/watch?v=XsX3ATc3FbA",
         genres: ["k-pop", "pop"],
         year: 2019
-    },{
+    }, {
         title: "Candy Shop",
         artist: ["50 Cent"],
         featuredArtist: ["Olivia"],
@@ -23,23 +23,23 @@ const emojiItems = [
         musicVideo: "https://www.youtube.com/watch?v=SRcnnId15BA",
         genres: ["Dirty rap"],
         year: 2004
-    },{
+    }, {
         title: "Castle on the Hill",
         artist: ["Ed Sheeran"],
         featuredArtist: [],
         emojiImgs: "🏰🏔️🍺🏡🚙",
         musicVideo: "https://www.youtube.com/watch?v=K0ibBPhiaG0",
-        genres: ["pop","singer-songwriter"],
+        genres: ["pop", "singer-songwriter"],
         year: 2017
-    },{
+    }, {
         title: "Chasing Cars",
         artist: ["Snow Patrol"],
         featuredArtist: [""],
         emojiImgs: "🚗🏃🛌",
         musicVideo: "https://www.youtube.com/watch?v=GemKqzILV4w",
-        genres: ["alternative", "indie" ,"rock"],
+        genres: ["alternative", "indie", "rock"],
         year: 2006
-    },{
+    }, {
         title: "Cheetah Sisters",
         artist: ["The Cheetah Girls"],
         featuredArtist: [],
@@ -47,7 +47,7 @@ const emojiItems = [
         musicVideo: "https://www.youtube.com/watch?v=RXt_lya0gto",
         genres: ["soundtrack", "pop"],
         year: 2003
-    },{
+    }, {
         title: "Cry Me A River",
         artist: ["Justin Timberlake"],
         featuredArtist: [],
@@ -63,7 +63,7 @@ const emojiItems = [
         musicVideo: "http://www.youtube.com/watch?v=eH3giaIzONA",
         genres: ["pop", "rb"],
         year: 1987
-    },{
+    }, {
         title: "I Write Sins Not Tragedies",
         artist: ["Panic! at the Disco"],
         featuredArtist: [],
@@ -71,7 +71,7 @@ const emojiItems = [
         musicVideo: "https://www.youtube.com/watch?v=vc6vs-l5dkc",
         genres: ["emo", "pop", "punk"],
         year: 2005
-    },{
+    }, {
         title: "Man's Not Hot",
         artist: ["Big Shaq"],
         featuredArtist: [],
@@ -79,7 +79,7 @@ const emojiItems = [
         musicVideo: "https://www.youtube.com/watch?v=3M_5oYU-IsU",
         genres: ["hip-hop", "comedy", "rap"],
         year: 2017
-    },{
+    }, {
         title: "Nine in the Afternoon",
         artist: ["Panic! at the Disco"],
         featuredArtist: [],
@@ -87,7 +87,7 @@ const emojiItems = [
         musicVideo: "https://www.youtube.com/watch?v=yCto3PCn8wo",
         genres: ["emo", "pop", "punk"],
         year: 2008
-    },{
+    }, {
         title: "Old Town Road",
         artist: ["Lil Nas X"],
         featuredArtist: ["Billy Ray Cyrus"],
@@ -95,15 +95,15 @@ const emojiItems = [
         musicVideo: "https://www.youtube.com/watch?v=w2Ov5jzm3j8",
         genres: ["hip-hop", "pop"],
         year: 2019
-    },{
+    }, {
         title: "Party in the USA",
         artist: ["Miley Cyrus"],
         featuredArtist: [],
         emojiImgs: "🎉🎊🇺🇲❤️🤍💙",
-        musicVideo:"https://www.youtube.com/watch?v=M11SvDtPBhA",
+        musicVideo: "https://www.youtube.com/watch?v=M11SvDtPBhA",
         genres: ["pop"],
         year: 2009
-        },{
+    },{
         title: "Rocketman",
         artist: ["Elton John"],
         featuredArtist: [],
@@ -119,7 +119,7 @@ const emojiItems = [
         musicVideo: "https://www.youtube.com/watch?v=WDAd0S92Uko",
         genres: ["pop"],
         year: 2015
-    }, , {
+    },{
         title: "Suit & Tie",
         artist: ["Justin Timberlake"],
         featuredArtist: ["JAY-Z"],
@@ -159,5 +159,29 @@ const emojiItems = [
         musicVideo: "https://www.youtube.com/watch?v=uhG-vLZrb-g",
         genres: ["rock"],
         year: 2004
+    },{
+        title: "thank u, next",
+        artist: ["Ariana Grande"],
+        featuredArtist: [],
+        emojiImgs: "🙌🏼💔🙌🏼💔🙌🏼💁🏻‍♀️",
+        musicVideo: "https://www.youtube.com/watch?v=gl1aHhXnN1k",
+        genres: ["pop"],
+        year: 2019
+    }, {
+        title: "Truckin",
+        artist: ["Grateful Dead"],
+        featuredArtist: [],
+        emojiImgs: "🚚🎳🚓",
+        musicVideo: "https://youtu.be/pafY6sZt0FE",
+        genres: ["rock", "alternative"],
+        year: 2019
+    }, {
+        title: "Thriller",
+        artist: ["Michael Jackson"],
+        featuredArtist: [],
+        emojiImgs: "🌃🕺🏿🧟‍♂️🧟‍♀️💃🏾",
+        musicVideo: "https://www.youtube.com/watch?v=4V90AmXnguw",
+        genres: ["pop"],
+        year: 1984
     }
 ]
